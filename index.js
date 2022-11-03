242514f3039cd82cf8f20279ddb7b0b922b1f8cc
kbleul <kbleulseged@gmail.com>
2022-11-03 09:31:14 -0400

    first commit 2

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use((req , res , next) => {
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
-    app.listen(process.env.PORT , () => { console.log("Server is running at " + process.env.PORT )})
+    app.listen(process.env.PORT || 3000, () => { console.log("Server is running at " + process.env.PORT )})
  })
   .catch(error => console.log(error))
 
