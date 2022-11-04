ef8c7e68671ef6f80d3bfd90a7a74e289447449d
kbleul <kbleulseged@gmail.com>
2022-11-04 05:44:58 -0400

    fix 5

--- index.js
+++ index.js
@@ -11,8 +11,13 @@ app.use(cors());
 
 app.use((req , res , next) => { 
     console.log(req.path, req.method , req.body)     
+    res.header("Access-Control-Allow-Origin","*")
+    res.header("Access-Control-Allow-Methods","POST")
+    res.header("Access-Control-Allow-Headers","Content-Type")
+
     next()
 })
+
 
 
 mongoose.connect(process.env.MONGO_URI)
